a9b38cb92d7782694f326eb74c6cf2d640c23c95
dibitss <dibitss@pm.me>
2021-10-01 20:51:28 +0300

    Removed creds and renamed

---
+++ Day 033 - API/project.py
@@ -0,0 +1,66 @@
+import requests
+import datetime as dt
+import time
+
+PASSWORD = ""
+USER_EMAIL = "@gmail.com"
+
+LOCAL_LATITUDE = 32.164860
+LOCAL_LONGITUDE = 34.844170
+
+
+def iss_is_close():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if LOCAL_LATITUDE -5 <= iss_latitude <= LOCAL_LATITUDE +5 and LOCAL_LONGITUDE -5 < iss_longitude < LOCAL_LATITUDE +5:
+        return True
+    
+    return False
+
+
+def send_email():
+    import smtplib
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+
+        connection.login(user=USER_EMAIL, password=PASSWORD)
+        connection.sendmail(
+            from_addr=USER_EMAIL, 
+            to_addrs=USER_EMAIL, 
+            msg=f"Subject:Look Up!\n\nThe ISS should be visible in the sky about now"
+            )
+
+
+def is_dark_outside():
+    parameters = {
+        "lat": LOCAL_LATITUDE,
+        "lng": LOCAL_LONGITUDE,
+        "formatted": 0
+    }
+
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+
+    data = response.json()
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+
+    time_now = dt.datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+    return False
+
+while True:
+    if is_dark_outside() and iss_is_close():    
+        send_email()
+    
+    time.sleep(60)
+
+
